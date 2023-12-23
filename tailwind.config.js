4744c5bbd62269244f1b428878cb499a8589ba62
DanuchaP <danucha.phol@gmail.com>
2023-12-23 17:47:56 +0700

    update 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         primary: "#CC2424",
         "wd-white": "#FFFFFF",
         "wd-grey": "#999999",
+        "wd-brand": "#898989",
         "wd-darkgrey": "#2D2D2D",
         "wd-black": "#080808",
       },
